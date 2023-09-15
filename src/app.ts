8086ed4f2b55c7017da7f812972094b18873314c
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2023-09-15 17:29:34 -0300

    Added cookie support for user identification

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
 import { meal } from './routes/meal'
 import { env } from './env/handle-env-variables'
+import cookie from '@fastify/cookie'
 
 const app = fastify({})
+
+app.register(cookie)
 
 app.register(meal, { prefix: '/meal' })
 
