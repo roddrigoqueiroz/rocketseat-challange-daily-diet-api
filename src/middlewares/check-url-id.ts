444b7fd55b0ec466b012a040a01245f4423f5949
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2023-09-19 17:09:20 -0300

    Added session_id checking

--- src/middlewares/check-url-id.ts
+++ src/middlewares/check-url-id.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { knex } from '../database'
 
 export async function checkUrlId(request: FastifyRequest, reply: FastifyReply) {
   const expectedId = z.object({
@@ -11,5 +12,12 @@ export async function checkUrlId(request: FastifyRequest, reply: FastifyReply) {
   if (!parse.success)
     return reply.code(401).send('ERROR: Unauthorized. Wrong ID')
 
-  return parse.data.id
+  const checkId = await knex('meal')
+    .where('id', parse.data.id)
+    .andWhere('session_id', request.cookies.session_id)
+
+  if (checkId.length === 0)
+    return reply
+      .code(401)
+      .send('ERROR: Unauthorized. This ID does not belongs to you')
 }
